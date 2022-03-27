5fe41d162bb54a962d6736b420a96d2e233b0070
Rishabh Malhotra <rishabhmr1995@gmail.com>
2022-03-27 20:25:33 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,6 +1,8 @@
-import flask
-app = flask.Flask(__name__)
+import os
+from flask import Flask
+
+app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return "Hello World. This app is running"
+def hello():
+    return "I love this site!!!"
