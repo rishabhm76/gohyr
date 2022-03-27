3639e7752adf476e373e92f89abcf5e91d3a3a95
Rishabh Malhotra <rishabhmr1995@gmail.com>
2022-03-27 20:35:18 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -6,3 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "I love this site!!!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
