cc2374ff76fca1f3a15314673bb56a63328481cb
Rishabh Malhotra <rishabhmr1995@gmail.com>
2022-04-12 22:33:10 +0530

    Update views.py

--- upload/views.py
+++ upload/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+import docx
+import pdfplumber
+
 # Create your views here.
 @csrf_exempt
 def index(request):
@@ -17,8 +20,35 @@ def my_program(files):
     fileList = []
     for f in files.keys():
         newFile = {}
-        newFile['fileName'] = files[f].name
-        newFile['content'] = files[f].read().decode("utf-8")
-        fileList.append(newFile)
+        
+        if files[f].name.endswith('.docx'):
+            doc = docx.Document(files[f])
+            text = ""
+            fullText = []
+            for para in doc.paragraphs:
+                fullText.append(para.text)
+                text = '\n'.join(fullText)
+        
+            newFile['fileName'] = files[f].name
+            newFile['content'] = text
+                
+        elif files[f].name.endswith('.pdf'):
+            text = ""
+            fullText = []
+            with pdfplumber.open(files[f]) as pdf:
+                for page in pdf.pages:
+                    fullText = page.extract_text()
+                    text += '\n' + fullText
+                
+            newFile['fileName'] = files[f].name
+            newFile['content'] = text
+            
+        else:
+            
+            newFile['fileName'] = files[f].name
+            newFile['content'] = files[f].read().decode("utf-8")
+            fileList.append(newFile)
+                  
+        
     #print(x)
     return fileList
