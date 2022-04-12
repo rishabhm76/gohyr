16dead9c62473d2ad545d1354c66b0153302b725
Rishabh Malhotra <rishabhmr1995@gmail.com>
2022-04-12 22:53:09 +0530

    Update views.py

--- upload/views.py
+++ upload/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+pip install python-docx
 import python-docx
 import pdfplumber
 
