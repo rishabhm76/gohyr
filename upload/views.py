b617af433d53ea96f2d9ea8fd1696d96c1e6211e
Rishabh Malhotra <rishabhmr1995@gmail.com>
2022-05-28 18:20:35 +0530

    Update views.py

--- upload/views.py
+++ upload/views.py
@@ -262,10 +262,10 @@ def my_program(files):
     #calculating score acquired by resume for this JD
 
     score = (len(final_list)/len(req_list))*100
-
+'''
     df = df.append({"JD Filepath" : path, "Resume Filepath" : var, 'Score' : score, "Word Count Resume" : wordCount_Resume,
                     "Word Count JD" : wordCount_JD, "Contains Mobile" : bonus_mobile, "Contains Email" : bonus_email,
                     "Contains LinkedIn" : bonus_linkedIn, "Soft Skills" : soft_skills, "Hard Skills" : hard_skills,
                    "Certifications" : certifications}, ignore_index = True)
-
+'''
     return score
