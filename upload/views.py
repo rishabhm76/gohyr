67b367723537a33bf74597304079a7c4837e32fd
Rishabh Malhotra <rishabhmr1995@gmail.com>
2022-05-28 18:33:37 +0530

    Update views.py

--- upload/views.py
+++ upload/views.py
@@ -266,4 +266,4 @@ def my_program(files):
                     "Contains LinkedIn" : bonus_linkedIn, "Soft Skills" : soft_skills, "Hard Skills" : hard_skills,
                    "Certifications" : certifications}, ignore_index = True)
 
-    return score
+    return df
