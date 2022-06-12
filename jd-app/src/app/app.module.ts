81252fad58e29c935234312521c26b6fa702cec9
sunny singh <sunnysingh.vpcoe@gmail.com>
2022-06-13 00:23:25 +0530

    home added

--- jd-app/src/app/app.module.ts
+++ jd-app/src/app/app.module.ts
@@ -5,10 +5,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { UploadComponent } from './upload/upload.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    UploadComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
